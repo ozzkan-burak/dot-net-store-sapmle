4234c058d007f86dc0d6530b00301ad83b08e058
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-05-18 23:49:21 +0300

    episode45

--- Repositories/Contracts/IRepositoryManager.cs
+++ Repositories/Contracts/IRepositoryManager.cs
@@ -3,6 +3,7 @@ namespace Repositories.Contracts
   public interface IRepositoryManager
   {
     public IProductRepository Product { get; }
+    public ICategoryRepository Category { get; }
     void Save();
   }
 }
