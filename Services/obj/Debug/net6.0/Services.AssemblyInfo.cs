bbb421d355bd7bb913b06c67f86316f1724da4cc
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-05-30 00:53:57 +0300

    episode56

--- Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0e53f9b9ebfce1a75e74157b426e9b5e70dac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e76fe383c0b1448b2acaeb0c9b5114e6249ed68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
