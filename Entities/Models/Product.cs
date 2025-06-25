c8c67aa8a5b16d408f858beac4004a6c9db1e5a9
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-06-25 23:58:13 +0300

    episode74

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -1,8 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities.Models;
+
 public class Product
 {
   public int ProductId { get; set; }
+  [Required(ErrorMessage = "Product name is required.")]
   public String? ProductName { get; set; }
   public decimal Price { get; set; }
 }
