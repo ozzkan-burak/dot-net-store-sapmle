c50a24a8f24b4983a2860aa46c9ae24d609468f3
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-07-29 20:54:05 +0300

    episode84

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -6,9 +6,7 @@ namespace Entities.Models;
 public class Product
 {
   public int ProductId { get; set; }
-  [Required(ErrorMessage = "Product name is required.")]
-  public String? ProductName { get; set; }
-  [Required(ErrorMessage = "Price is required.")]
+  public String? ProductName { get; set; } = String.Empty;
   public decimal Price { get; set; }
   public int CategoryId { get; set; }
   public Category? Category { get; set; }
