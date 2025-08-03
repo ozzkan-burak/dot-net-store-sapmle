b0531f1d0ff6502214707e2ab0267530d421cff9
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-08-03 16:28:55 +0300

    episode89

--- StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8484e1c897d746340a6ff42b09f1aa9808da2ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651f8cc1ad64c23b2eabd94f90275bf95c42f586")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
