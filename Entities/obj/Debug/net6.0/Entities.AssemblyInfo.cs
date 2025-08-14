568748eaff81238da0c91c8e6a535ff9979705a6
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-08-14 22:51:40 +0300

    episode93

--- Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234d36622fb580a76cb125138e528d0ed5910f3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aef6c9d5964326713b1a811116ff070b85b4d84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234d36622fb580a76cb125138e528d0ed5910f3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aef6c9d5964326713b1a811116ff070b85b4d84")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
