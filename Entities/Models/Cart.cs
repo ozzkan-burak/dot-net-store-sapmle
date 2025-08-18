8b8ad12a443af51ffa818d5cd3f50882e052f847
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-08-18 21:41:51 +0300

    episode101

---
+++ Entities/Models/Cart.cs
@@ -0,0 +1,59 @@
+
+namespace Entities.Models
+{
+  public class Cart
+  {
+    public List<CartLine> Lines { get; set; }
+
+    public Cart()
+    {
+      Lines = new List<CartLine>();
+    }
+
+    public void AddItem(Product product, int quantity)
+    {
+      CartLine? line = Lines.Where(l => l.Product.ProductId.Equals(product.ProductId)).FirstOrDefault();
+      if (line == null)
+      {
+        line = new CartLine { Product = product, Quantity = quantity };
+        Lines.Add(line);
+      }
+      else
+      {
+        line.Quantity += quantity;
+      }
+    }
+
+    public void RemoveLine(Product product)
+    {
+      CartLine? line = Lines.Where(l => l.Product.ProductId.Equals(product.ProductId)).FirstOrDefault();
+      if (line != null)
+      {
+        Lines.Remove(line);
+      }
+    }
+
+    public void RemoveItem(Product product, int quantity = 1)
+    {
+      CartLine? line = Lines.Where(l => l.Product.ProductId.Equals(product.ProductId)).FirstOrDefault();
+      if (line != null)
+      {
+        line.Quantity -= quantity;
+        if (line.Quantity <= 0)
+        {
+          Lines.Remove(line);
+        }
+      }
+    }
+
+    public decimal TotalValue => Lines.Sum(e => e.Product.Price * e.Quantity);
+
+    public int TotalCount => Lines.Sum(e => e.Quantity);
+
+    public void ClearCart()
+    {
+      Lines.Clear();
+    }
+
+  }
+}
