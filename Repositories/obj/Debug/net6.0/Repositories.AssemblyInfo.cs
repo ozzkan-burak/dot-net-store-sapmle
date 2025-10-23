162416a8d677b1905e21b7e077b9b72d73ad29cf
Burak Özkan <brk.ozzkan@outlook.com.tr>
2025-10-24 00:25:12 +0300

    episode129

--- Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2adad99bd9a8a5afc3889119ce574d41a1c25c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4917a018a2f0af282c41fe1379cabdd221fbf479")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
